8a2cb8d82eeb92b6c224f82f0fae86da6cc81d13
ChaOS <mail@sp8de.com>
2018-06-28 15:31:42 +0300

    neo related serivices

---
+++ src/Sp8de.NeoServices/NeoKeySecret.cs
@@ -0,0 +1,16 @@
+﻿using Sp8de.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sp8de.NeoServices
+{
+    public class NeoKeySecret : IKeySecret
+    {
+        public KeyType Type => KeyType.Neo;
+
+        public string PrivateKey { get; set; }
+
+        public string PublicAddress { get; set; }
+    }
+}
---
+++ src/Sp8de.NeoServices/NeoKeySecretGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Sp8de.Common.Interfaces;
+using System.Security.Cryptography;
+
+namespace Sp8de.NeoServices
+{
+    public class NeoKeySecretGenerator: IKeySecretGenerator
+    {
+        public IKeySecret Generate()
+        {
+            using (CngKey key = CngKey.Create(CngAlgorithm.ECDsaP256, null, new CngKeyCreationParameters { ExportPolicy = CngExportPolicies.AllowPlaintextArchiving }))
+            {
+                var privateKey = key.Export(CngKeyBlobFormat.EccPrivateBlob);
+                var account = new NeoModules.KeyPairs.KeyPair(privateKey);
+
+                return new NeoKeySecret()
+                {
+                    PrivateKey = account.Export(),
+                    PublicAddress = account.PublicKey.ToString()
+                };
+            }
+        }
+    }
+}
