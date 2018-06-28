83d8dd4cc8475a11c41fcb6b3c43236413d1e884
ChaOS <mail@sp8de.com>
2018-06-28 15:31:24 +0300

    eth related services

---
+++ src/Sp8de.EthServices/EthKeySecret.cs
@@ -0,0 +1,13 @@
+﻿using Sp8de.Common.Interfaces;
+
+namespace Sp8de.EthServices
+{
+    public class EthKeySecret : IKeySecret
+    {
+        public KeyType Type => KeyType.Eth;
+
+        public string PrivateKey { get; set; }
+
+        public string PublicAddress { get; set; }
+    }
+}
---
+++ src/Sp8de.EthServices/EthKeySecretGenerator.cs
@@ -0,0 +1,20 @@
+﻿using Nethereum.Signer;
+using Sp8de.Common.Interfaces;
+using System;
+
+namespace Sp8de.EthServices
+{
+    public class EthKeySecretGenerator : IKeySecretGenerator
+    {
+        public IKeySecret Generate()
+        {
+            var key = EthECKey.GenerateKey();
+
+            return new EthKeySecret()
+            {
+                PrivateKey = key.GetPrivateKey(),
+                PublicAddress = key.GetPublicAddress()
+            };
+        }
+    }
+}
