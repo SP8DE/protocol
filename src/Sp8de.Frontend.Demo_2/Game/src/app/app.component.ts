8e3432e3d3c5b0a7478227f653e68f29df27f50a
pavelzubov <pasha224@yandex.ru>
2018-08-01 16:28:17 +0300

    Change Frontend Demo_2 files

--- src/Sp8de.Frontend.Demo_2/Game/src/app/app.component.ts
+++ src/Sp8de.Frontend.Demo_2/Game/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Sp8deCrypto} from 'sp8de.crypto';
-import {DemoGameService, GameFinishRequest, GameStartRequest, GameStartResponse} from '../api';
+import {DemoGameService, GameFinishRequest, GameStartRequest, GameStartResponse} from './api';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 
@@ -14,8 +14,7 @@ export class AppComponent implements OnInit {
   private pubKey: string;
   private seed: number;
   private sign: string;
-  private gameId: string;
-  private items: any[];
+  private startResponse: GameStartResponse;
   private requestStart: GameStartRequest = {
     type: 'Dice',
     bet: [1],
@@ -38,40 +37,44 @@ export class AppComponent implements OnInit {
     this.pubKey = this.sp8deCrypto.getPubKey(this.privateKey);
     this.seed = this.sp8deCrypto.generateSeed();
     this.sign = this.sp8deCrypto.sing(this.privateKey, this.seed, this.requestStart.nonce).sign;
-    this.startGame(this.sign, this.pubKey).subscribe(
+    console.group('Start');
+    this.startGame({sign: this.sign, pubKey: this.pubKey}).subscribe(
       res => {
-        console.log('start', res);
-        this.endGame(this.gameId, this.sign, this.seed, this.pubKey)
+        console.warn('Start response', res);
+        console.groupEnd();
+        console.group('End');
+        this.endGame({gameId: this.startResponse.gameId, sign: this.sign, seed: this.seed, pubKey: this.pubKey})
           .subscribe(
             result => {
-              console.log('end', result);
-              console.log('Check win: ', this.checkWin(result.sharedSeedArray, result.winNumbers[0]));
+              console.warn('End response', result);
+              console.log('Check win:', this.checkWin(result.sharedSeedArray, result.winNumbers[0]));
+              console.groupEnd();
             },
             error => {
-              console.log(error.message);
+              console.error(error.message);
             });
       },
-      error => console.log(error.message)
+      error => console.error(error.message)
     );
   }
 
   private checkWin(array: number[], winNumber: number): boolean {
+    console.log(this.sp8deCrypto.getRandomFromArray(array, 1, 6),winNumber)
     return this.sp8deCrypto.getRandomFromArray(array, 1, 6) === winNumber;
   }
 
-  public startGame(sign: string, pubKey: string): Observable<GameStartResponse> {
-    return this.api.apiDemoGameStartPost(this.createRequestStart(this.requestStart, sign, pubKey))
+  public startGame(parameters: parametersStartGame): Observable<GameStartResponse> {
+    return this.api.apiDemoGameStartPost(this.createRequestStart(this.requestStart, parameters))
       .pipe(
         map(
           item => {
-            this.items = item.items;
-            this.gameId = item.gameId;
+            this.startResponse = item;
             return item;
           }));
   }
 
-  public endGame(gameId: string, sign: string, seed: number, pubKey: string): Observable<any> {
-    return this.api.apiDemoGameEndPost(this.createRequestEnd(this.requestEnd, seed, gameId, sign, pubKey))
+  public endGame(parameters: parametersEndGame): Observable<any> {
+    return this.api.apiDemoGameEndPost(this.createRequestEnd(this.requestEnd, parameters))
       .pipe(
         map(item => {
           // some actions
@@ -80,33 +83,27 @@ export class AppComponent implements OnInit {
       );
   }
 
-  private createRequestEnd(request: GameFinishRequest, seed: number, gameId: string, sign: string, pubKey: string): GameFinishRequest {
-    if (!request ||
-      !seed ||
-      !pubKey ||
-      !gameId ||
-      !sign) {
-      console.error('Invalid parameters');
-      return;
+  private createRequestEnd(request: GameFinishRequest, parameters: parametersEndGame): GameFinishRequest {
+    for (let parameter in parameters) {
+      if (!parameters[parameter]) {
+        console.error('Invalid parameters');
+        return;
+      }
+      request[parameter] = parameters[parameter];
     }
-    request.gameId = gameId;
-    request.seed = seed;
-    request.sign = sign;
-    request.pubKey = pubKey;
-    console.log('end request', request);
+    console.info('End request', request);
     return request;
   }
 
-  private createRequestStart(request: GameStartRequest, sign: string, pubKey: string): GameStartRequest {
-    if (!request ||
-      !pubKey ||
-      !sign) {
-      console.error('Invalid parameters');
-      return;
+  private createRequestStart(request: GameStartRequest, parameters: parametersStartGame): GameStartRequest {
+    for (let parameter in parameters) {
+      if (!parameters[parameter]) {
+        console.error('Invalid parameters');
+        return;
+      }
+      request[parameter] = parameters[parameter];
     }
-    request.sign = sign;
-    request.pubKey = pubKey;
-    console.log('start request', request);
+    console.info('Start request', request);
     return request;
   }
 
@@ -117,4 +114,16 @@ export class AppComponent implements OnInit {
     }
     return result;
   }
+}
+
+interface parametersEndGame {
+  gameId: string,
+  sign: string,
+  seed: number,
+  pubKey: string
+}
+
+interface parametersStartGame {
+  sign: string,
+  pubKey: string
 }
--- src/Sp8de.Frontend.Demo_2/Game/src/app/app.module.ts
+++ src/Sp8de.Frontend.Demo_2/Game/src/app/app.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {Sp8deCrypto} from 'sp8de.crypto';
 import {AppComponent} from './app.component';
-import {ApiModule} from '../api';
+import {ApiModule} from './api';
 
 @NgModule({
   declarations: [
