d3f50bf74bc88e95b0d8eec2c533eac93d269c42
pavelzubov <pasha224@yandex.ru>
2018-08-24 09:31:58 +0300

    Add user

---
+++ src/Sp8de.Frontend.Demo_2/Game/src/app/game/user/user.component.ts
@@ -0,0 +1,26 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {User} from "../../types";
+import {UserService} from "../../services/user.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.sass']
+})
+export class UserComponent implements OnInit {
+  @Input() public user: User;
+  constructor(public userService: UserService,
+              public router: Router) { }
+
+  ngOnInit() {
+  }
+
+  public toFixed(val: number): string {
+    return val? val.toFixed(2) : '0';
+  }
+  public logOut(): void {
+    this.userService.logOut();
+    this.router.navigate(['./login']);
+  }
+}
