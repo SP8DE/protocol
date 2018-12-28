bfbc30097f649629b0e2515502a7eb15417eab4f
Ilya Ermoshin <ilya.ermoshin123@gmail.com>
2018-12-28 15:30:06 +0300

    Create SendMailModel.cs

---
+++ src/Sp8de.Common/Utils/Models/SendMailModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sp8de.Common.Utils.Models
+{
+    /// <summary>
+    /// Model for SMTP mail sending properties
+    /// </summary>
+    public class SendMailModel
+    {
+        /// <summary>
+        /// SMTP server address
+        /// </summary>
+        public string SmtpServer { get; set; }
+        /// <summary>
+        /// SMTP Server Port
+        /// </summary>
+        public int SmtpServerPort { get; set; }
+        /// <summary>
+        /// SMTP server password
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// E-mail sender address
+        /// </summary>
+        public string From { get; set; }
+        /// <summary>
+        /// E-mail receiver address
+        /// </summary>
+        public string MailTo { get; set; }
+        /// <summary>
+        /// E-mail subject
+        /// </summary>
+        public string Subject { get; set; }
+        /// <summary>
+        /// E-mail body message
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// E-mail attach file content
+        /// </summary>
+        public string AttachFile { get; set; }
+    }
+}
