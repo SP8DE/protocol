865b968f7a89148d5ff126d4f32b52a5f5d3a224
iddcud <iddcud@gmail.com>
2018-12-28 17:15:49 +0300

    websocket handler

---
+++ src/Sp8de.Explorer/WebSocketManager/WebSocketHandler.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sp8de.Explorer.Api.WebSocketManager
+{
+    public abstract class WebSocketHandler
+    {
+        protected WebSocketConnectionManager WebSocketConnectionManager { get; set; }
+
+        public WebSocketHandler(WebSocketConnectionManager webSocketConnectionManager)
+        {
+            WebSocketConnectionManager = webSocketConnectionManager;
+        }
+
+        public virtual async Task OnConnected(WebSocket socket)
+        {
+            WebSocketConnectionManager.AddSocket(socket);
+        }
+
+        public virtual async Task OnDisconnected(WebSocket socket)
+        {
+            await WebSocketConnectionManager.RemoveSocket(WebSocketConnectionManager.GetId(socket));
+        }
+
+        public async Task SendMessageAsync(WebSocket socket, string message)
+        {
+            if (socket.State != WebSocketState.Open)
+                return;
+            try
+            {
+                await socket.SendAsync(buffer: new ArraySegment<byte>(array: Encoding.ASCII.GetBytes(message),
+                                                                  offset: 0,
+                                                                  count: message.Length),
+                                   messageType: WebSocketMessageType.Text,
+                                   endOfMessage: true,
+                                   cancellationToken: CancellationToken.None);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+        public async Task SendMessageAsync(string socketId, string message)
+        {
+            try
+            {
+                await SendMessageAsync(WebSocketConnectionManager.GetSocketById(socketId), message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public async Task SendMessageToAllAsync(string message)
+        {
+            try
+            {
+                foreach (var pair in WebSocketConnectionManager.GetAll())
+                {
+                    if (pair.Value.State == WebSocketState.Open)
+                        await SendMessageAsync(pair.Value, message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        //TODO - decide if exposing the message string is better than exposing the result and buffer
+        public abstract Task ReceiveAsync(WebSocket socket, WebSocketReceiveResult result, byte[] buffer);
+    }
+}
