2d628e5f51869557f44f97fc5d1b514d352c6710
A16ert <>
2018-12-28 20:06:51 +0300

    MIF deserializer

---
+++ src/Sp8de.EthServices/Model/ConstructorMIF.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace Sp8de.EthServices.Model
+{
+    public class ConstructorMIF
+    {
+        public Parameter[] InputParameters { get; set; }
+    }
+}
