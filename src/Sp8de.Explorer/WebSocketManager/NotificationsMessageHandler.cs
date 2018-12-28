e2dd4b481f786df777adb8d3c8bbc5dcf8eb6dcd
iddcud <iddcud@gmail.com>
2018-12-28 17:13:18 +0300

    websocket manager

---
+++ src/Sp8de.Explorer/WebSocketManager/NotificationsMessageHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sp8de.Explorer.Api.WebSocketManager
+{
+    public class NotificationsMessageHandler
+    {
+    }
+}
