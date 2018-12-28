43af8594928f2c4c6090d87d49de1f89fc842837
iddcud <iddcud@gmail.com>
2018-12-28 17:20:05 +0300

    websocket manager extensions

--- src/Sp8de.Explorer/WebSocketManager/WebSocketHandler.cs
+++ src/Sp8de.Explorer/WebSocketManager/WebSocketHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Sp8de.Explorer.Api.WebSocketManager
