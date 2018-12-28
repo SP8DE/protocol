0ce68e15839ecfc8b3e8d83c7594f5d31bb3ef6e
A16ert <>
2018-12-28 17:23:50 +0300

    dictionary extensions

---
+++ src/Sp8de.Common/Utils/DictionaryExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sp8de.Common.Utils
+{
+    public static class DictionaryExtensions
+    {
+        public static string ToQueryString(this IDictionary<string, string> values)
+        {
+            return string.Join("&", values.Select(x => $"{x.Key}={x.Value}"));
+        }
+    }
+}
